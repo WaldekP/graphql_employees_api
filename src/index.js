08e32d2a78677d0720a4fec6972fd46a7e757601
Waldek <waldek.pieniak@gmail.com>
2019-12-04 00:14:28 +0100

    Graphql server initialized and first resolvers and types implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import 'dotenv/config';
+import express from 'express'
+import { ApolloServer, gql } from "apollo-server-express";
 
-console.log('blabla-lllaaa', process.env.MY_SECRET)
+const app = express()
+
+const schema = gql`
+    type Query {
+        me: User
+        you: User
+        employee: Employee
+    }
+    type User {
+        username: String!
+    }
+    type Employee {
+        username: String!
+        lastname: String!
+    }
+`;
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Waldek Pieniak',
+            };
+        },
+        you: () => {
+            return {
+                username: "Kajeczka Pierdeczka"
+            }
+        },
+        employee: () => {
+            return {
+                username: 'Jan',
+                lastname: 'Sniezek'
+            }
+        }
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+})
+
+app.get('/', (req, res) => {
+    res.send('Cos tam')
+})
+
+server.applyMiddleware({ app, path: '/graphql'})
+
+app.listen(4000, () => console.log('listening'))
